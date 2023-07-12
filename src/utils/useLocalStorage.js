4c067f4be3a4cd769a3fddf0e5b7b84e9978a333
PsychX0X <sashko1516@mail.ru>
2023-07-12 16:45:51 +0300

    fixed a bug with setting the default theme

--- src/utils/useLocalStorage.js
+++ src/utils/useLocalStorage.js
@@ -3,8 +3,7 @@ import { useState, useEffect } from 'react';
 function getStorageValue(key, defaultValue) {
 	// getting stored value
 	const saved = localStorage.getItem(key);
-	const initial = JSON.parse(saved);
-	return initial || defaultValue;
+	return saved ?? defaultValue;
 }
 
 export const useLocalStorage = (key, defaultValue) => {
